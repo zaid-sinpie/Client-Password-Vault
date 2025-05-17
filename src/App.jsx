1fbb76748d604bda18684ff678fbbdc808d93e07
zaid-sinpie <zaidmasuldar@gmail.com>
2025-05-17 14:23:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+    <p>Password Vault</p>
+    </>
+  )
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "public/manifest.json", dest: "." },
+        { src: "public/background.js", dest: "." },
+        { src: "public/contentScript.js", dest: "." }
+      ]
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
